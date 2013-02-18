1e16f1618f04ba19c6c36ee4044cc8f86e3687f4
Vitaly Lischenko <lischenko@gmail.com>
2013-02-18 22:58:10 +0400

    Fixed typos in flag descriptions

--- own/src/config/config.go
+++ own/src/config/config.go
@@ -13,8 +13,8 @@ var cfg Config
 
 func init() {
 	flag.StringVar(&cfg.Lang, "lang", "en", "Language of the Wikipedia to be queried")
-	flag.BoolVar(&cfg.CleanupLinks, "cleanupLinks", true, "Wheter or not to clean-up links")
-	flag.BoolVar(&cfg.StripCategories, "stripCategories", true, "Wheter or not to strip categories")
+	flag.BoolVar(&cfg.CleanupLinks, "cleanupLinks", true, "Whether or not to clean-up links")
+	flag.BoolVar(&cfg.StripCategories, "stripCategories", true, "Whether or not to strip categories")
 	flag.BoolVar(&cfg.Raw, "raw", false, "Supresses all post-processing")
 
 	flag.Parse()
